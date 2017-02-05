e668b70d3d4b3d2db10ac821e4e76d5056a23478
ryu19j <ryu.19j.5@gmail.com>
2017-02-05 21:35:06 +0900

    checkboxとラジオお試し。

--- vue-cli-sample/src/components/Input.vue
+++ vue-cli-sample/src/components/Input.vue
@@ -10,6 +10,19 @@
       <br>
       <textarea v-model="text" placeholder="入力してください。"></textarea>
     </div>
+    <div id="checkbox">
+      <input type="checkbox" id="checkbox" v-model="checked">
+      <label for="checkbox">{{checked}}</label>
+    </div>
+    <div id="radio">
+      <input type="radio" id="radio-one" value="綾鷹" v-model="picked">
+      <label for=" radio-one">綾鷹</label>
+       <br>
+      <input type="radio" id="radio-two" value="その他の何か" v-model="picked">
+      <label for="radio-two">その他の何か</label>
+      <br>
+        <span>選ばれたのは、{{picked}}でした。</span>
+    </div>
   </div>
 </template>
 
@@ -18,7 +31,10 @@ export default {
   data: function () {
     return {
       message: '',
-      text: ''
+      text: '',
+      checked: true,
+      picked: ''
+
     }
   }
 }

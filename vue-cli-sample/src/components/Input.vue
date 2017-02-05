5f7fd81bbbe0c5ed4e74aec77d547e48f86737d0
ryu19j <ryu.19j.5@gmail.com>
2017-02-05 18:43:16 +0900

    textareaお試し。

--- vue-cli-sample/src/components/Input.vue
+++ vue-cli-sample/src/components/Input.vue
@@ -1,8 +1,15 @@
 <template>
-  <div id="input" >
-    <p>入力欄</p>
-    <input v-model="message" />
-    <p>入力されているのは→「 {{ message }}」</p>
+  <div id="input"> 
+    <div id="textbox" >
+      <p>入力欄</p>
+      <input v-model="message" />
+      <p>入力されているのは→「 {{ message }}」</p>
+    </div>
+    <div id="textarea" >
+      <p style="white-space: pre">{{ text }}</p>
+      <br>
+      <textarea v-model="text" placeholder="入力してください。"></textarea>
+    </div>
   </div>
 </template>
 
@@ -10,7 +17,8 @@
 export default {
   data: function () {
     return {
-      message: ''
+      message: '',
+      text: ''
     }
   }
 }

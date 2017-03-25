de173cd77d87f9dc6bfa1500dc1112a71df79044
ryu19j <ryu.19j.5@gmail.com>
2017-03-25 23:24:51 +0900

    ボタンのサンプル。

---
+++ vue-parts-collection/src/components/Button.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <button @click="count">ボタン</button>
+    <p>{{ counter }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    count: function (event) {
+      this.counter += 1
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
